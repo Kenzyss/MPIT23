0dad2a4f83015945eff8bf14fd7cf0eaac36bf1c
Kenzyss <mbrovkin007@gmail.com>
2023-10-26 18:51:39 +0200

    WORKING

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ def predict():
         if percent > max_percentage:
             max_percentage = percent
             best_answer = value
-            keys = list(dictionary.keys())
-            index = list(dictionary.values()).index(best_answer)
-            key = keys[index]
 
+    keys = list(dictionary.keys())
+    index = list(dictionary.values()).index(best_answer)
+    key = keys[index]
             # response = res(text, lemmatize(value), key
-            message = {"answer": key}
-        return message
+    message = {"answer": key}
+    return message
         # response = res(text, lemmatize(i), percent, key)
         # message = {"answer": response}
     # return message
