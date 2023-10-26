dd70b0c2d3a9ef249f555781025d4401abf38093
Kenzyss <mbrovkin007@gmail.com>
2023-10-26 14:06:50 +0200

    Lemmontize

--- chat.py
+++ chat.py
@@ -1,27 +1,68 @@
-lang = 1  # 0 - ru; 1 - eng
+import string
+import nltk
+from nltk.stem import WordNetLemmatizer
 
-text = {
-    1: ["1", [
-        "Национальный парк открыт для посещения 24/7 – круглосуточно и круглогодично. Визит-центр, расположенный на 14 км косы, открыт с 9 до 17 часов: с мая по сентябрь ежедневно, с октября по апрель – со среды по воскресенье. Полевой стационар кольцевания птиц открыт для посещения с апреля по октябрь с 10 до 17 часов",
-        "engText"]],
-    2: ["Как попасть на Куршскую косу? Мы приехали на КПП/в Зеленоградск – куда идти дальше?", [
-        "Для осмотра основных достопримечательностей на Куршской косе требуется транспорт. Можно перемещаться на автомобиле, туристическом или рейсовом автобусе. Есть парковки. Экскурсию в составе туристической группы нужно заказывать заранее в любой из калининградских турфирм, рейсовый автобус № 210 отправляется от вокзала в Зеленоградске, в летний сезон из Калининграда на косу ходит автобус № 593.",
-        "engText"]]
-}
+lemmatizer = WordNetLemmatizer()
+
+
+def lemmatize(text):
+    words = nltk.word_tokenize(text.lower())
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in words if word not in string.punctuation]
+    result = lemmatized_words
+    # print(result)
+    return result
+
+
+def response(answer):
+    return lemmatize(answer)
+
+
+def compare_word_lists(list1, list2, key):
+    max_p = 0
+    result = []
+    set1 = set(list1)
+    set2 = set(list2)
+
+    common_words = set1.intersection(set2)
+
+    if len(set1) == 0 or len(set2) == 0:
+        return 0.0
+    else:
+        max_len = max(len(list1), len(list2))
+        percentage = len(common_words) / max_len * 100
+        if percentage > max_p:
+            max_p = percentage
+
+    # print(list2)
+    # if len(result) > 0 and result is not None:
+    return key
+
+
+lang = "rus"  # 0 - ru; 1 - eng
+if lang == "rus":
+    dictionary = {
+        'Question': 'Answer',
+        "Нужно идти на кпп": 'Я приехал, что делать дальше?',
+    }
+else:
+    dictionary = {
+        'привет': 'Привет!',
+    }
+
+question = lemmatize(input("Введите ваш вопрос: "))
+for i in dictionary.values():
+    keys = list(dictionary.keys())
+    index = list(dictionary.values()).index(i)
+    key = keys[index]
+    print(compare_word_lists(question, lemmatize(i), key))
+
+# print(f"Процент совпадения: {result:.2f}%")
 
 
 # print(text.get(1)[1][lang])
 
 
-def get_response(msg):
-    return "Не нужно, билет действует трое суток при условии непрерывного пребывания на территории национального парка. Билет необходимо сохранять до момента выезда с Куршской косы."
-    # for k, v in text.items():
-    #     if v == msg:
-    #         return text.get(k)[1][lang]
-        # return text.get(1)[1][lang]
-
-    # return "I do not understand..."
-
+# return "I do not understand..."
 
 
 # if __name__ == "__main__":
